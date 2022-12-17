d5e46163909a615b8940184034c19ce329ebefd2
spaceinvader10 <54293311+spaceinvader10@users.noreply.github.com>
2022-12-16 16:03:02 -0800

    Implemented logic for the game. Add in Ui/ux next steps for the game. Need button and images for the visuals for the game.

--- scripts.js
+++ scripts.js
@@ -1,19 +1,28 @@
 let gameChoices =['rock', 'paper', 'scissor'];
 let gameRecord =[];
-let playerSign = false;
+let playerSign ="";
+let playerWin;
+let computerWin;
+let target = 'Player Wins!';
+let playerScore = 0;
+let computerScore = 0;
 
-while(playerSign === false) {
 
-    let sign = prompt("Choose Rock, Paper, or Scissor to proceed!");
+function playerSelection () {
+
+    let sign = prompt("Choose Rock, Paper, or Scissor to proceed! A total of five round will be played!");
         //want to see the user's response
         if(sign.toLowerCase() === 'rock'|| sign.toLowerCase() ==='paper' || sign.toLowerCase() ==='scissor') {
             playerSign = sign;
-            playerSelection(playerSign.toLowerCase());
-        } else {
-            alert('Please answer with one of the options.');
-    }
+            console.log('The player has selected ' + playerSign);
+            return playerSign.toLowerCase();
 
+        } else {
+            console.log('Please answer with one of the options.');
+        }
 }
+
+
 
 
 
@@ -27,8 +36,6 @@ function computerSelection (){
 
 }
 
-computerSelection();
-
 
 /*
 Generate a random computer choice with rock, paper and scissor stored in an array
@@ -36,12 +43,6 @@ Generate a random computer choice with rock, paper and scissor stored in an arra
 Have the player choose a choice also from the array
 */
 
-function playerSelection(choice){
-    choice = playerSign;
-    console.log('The player\'s choice is ' + choice);
-    return choice;
-
-}
 
 
 //compare the results of the player vs the computer and determine the winner
@@ -50,37 +51,55 @@ function playRound(playerSelection, computerSelection){
 
     while (gameRecord.length < 5){
         //while the rounds are less than five keep playing until there are 5 rounds
-
-        //loop needs to happen here or else infinite stuck
+        // console.log(gameRecord);
+         playerWin = playerSelection();
+         computerWin = computerSelection();
+        //loop needs to happen here or else infinite stuck, call the computerSelection and player Selection here
         
         //logic for the game
-        if (playerSelection === computerSelection){
+        if (playerWin === computerWin){
             console.log('It is a tie!')
-        } else if( playerSelection === 'rock' && computerSelection === 'scissor'){
+        } else if( playerWin === 'rock' && computerWin === 'scissor'){
             console.log('Player defeats computer with rock vs scissor');
             gameRecord.push('Player Wins!');
-        } else if( playerSelection === 'rock' && computerSelection === 'paper'){
+            playerScore++;
+        } else if( playerWin === 'rock' && computerWin === 'paper'){
             console.log('Computer defeats player with paper vs rock');
             gameRecord.push('Player Loses!');
-        } else if( playerSelection === 'scissor' && computerSelection === 'paper'){
+            computerScore++;
+        } else if( playerWin === 'scissor' && computerWin === 'paper'){
             console.log('Player defeats computer with scissor vs paper');
             gameRecord.push('Player Wins!');
-        } else if( playerSelection === 'scissor' && computerSelection === 'rock'){
+            playerScore++;
+        } else if( playerWin === 'scissor' && computerWin === 'rock'){
             console.log('Computer defeats player with rock vs scissor');
             gameRecord.push('Player Loses!');
-        }  else if( playerSelection === 'paper' && computerSelection === 'rock'){
+            computerScore++;
+        }  else if( playerWin === 'paper' && computerWin === 'rock'){
             console.log('Player defeats computer with paper vs rock');
             gameRecord.push('Player Wins!');
-        } else if( playerSelection === 'paper' && computerSelection === 'scissor'){
+            playerScore++;
+        } else if( playerWin === 'paper' && computerWin === 'scissor'){
             console.log('Computer defeats player with scissor vs paper');
             gameRecord.push('Player Loses!');
+            computerScore++;
         }
+
+        console.log('Round '+ (gameRecord.length) + ' Player: ' + playerScore + ' : Computer: ' + computerScore +'.');
+
     }
+
+// intergrated game logic into the if else
+   
+
     
+ //   console.log(gameRecord.length)
+    console.log( playerScore > computerScore ? 'Player has won the game!': 'Computer has won the game!');
+  
     
 
 }
 
 
-playRound(playerSelection(playerSign),computerSelection());
+playRound(playerSelection,computerSelection);
 //logic for rock paper scissors
