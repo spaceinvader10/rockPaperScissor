47764162b6949c79bbcce6c3479edd38a24803eb
spaceinvader10 <54293311+spaceinvader10@users.noreply.github.com>
2023-01-19 15:54:31 -0800

    Refactor previous code to display the information with great visual clarity and provided more information will need to implement game round logic

--- scripts.js
+++ scripts.js
@@ -8,31 +8,41 @@ let playerScore = 0;
 let computerScore = 0;
 let game = 5;
 let bsign = 'nonfate';
+let liveFeeder = document.getElementById('liveFeed');
+let liveFeeder2 = document.getElementById('liveFeed2');
+let outcome = document.querySelector('#matchOutcome');
+
 
 
 //game function should called the playround function and determine the amount of rounds needed for the game, need to integrate into its own function later
 let rock = document.getElementById('playRock');
 let paper = document.getElementById('playPaper');
 let scissor = document.getElementById('playScissor');
+let userStat = document.getElementById('userStats');
+let computerStat = document.getElementById('computerStats');
+let roundStat =  document.getElementById('round');
 
-rock.addEventListener('click', function(){
-    console.log('rock was selected ');
+rock.addEventListener('click', ()=>{
+    liveFeeder2.innerHTML= 'Player has selected rock.'
+    //console.log('rock was selected.');
     bsign = 'rock';
     return playRound(playerSelection(bsign));
 })
-paper.addEventListener('click', function(){
-    console.log('paper was selected ');
+paper.addEventListener('click', ()=>{
+    liveFeeder2.innerHTML ='Player has selected paper.';
+    // console.log('paper was selected ');
     bsign = 'paper';
     return playRound(playerSelection(bsign));
 })
 
 scissor.addEventListener('click', ()=>{
-    console.log('scissor was selected ');
+    liveFeeder2.innerHTML ='Player has selected scissor.';
+    // console.log('scissor was selected ');
     bsign = 'scissor';
     return playRound(playerSelection(bsign));
 })
 
-  //  return sign = 'rock';)
+//return sign = 'rock';)
 //console.log(bsign)
 
 
@@ -40,12 +50,16 @@ function playerSelection(sign) {
         //want to see the user's response
         if(sign.toLowerCase() === 'rock'|| sign.toLowerCase() ==='paper' || sign.toLowerCase() ==='scissor') {
             playerSign = sign;
-            console.log('The player has selected ' + playerSign);
+            // console.log('The player has selected ' + playerSign);
+            liveFeeder.innerHTML = 'The player has selected ' + playerSign;
             return playerSign.toLowerCase();
-
-        } else {
-            console.log('Please answer with one of the options.');
+            
         }
+        //  else {
+        //     liveFeeder.innerHTML ='Please answer with one of the options.';
+        //     // console.log('Please answer with one of the options.');
+        //     return null;
+        // }
 }
 
 
@@ -56,7 +70,9 @@ function computerSelection (){
 
     let random = Math.floor(Math.random()*3);
     let computerResponse = gameChoices[random];
-    console.log('The computer has selected '+ computerResponse);
+    
+    liveFeeder.innerHTML = 'Computer has selected '+ computerResponse;
+    // console.log('The computer has selected '+ computerResponse);
 
     return computerResponse;
 
@@ -75,13 +91,15 @@ Have the player choose a choice also from the array
 
 function playRound(player){
 
+
+
     if(player === undefined){
         console.log('failed player choice');
         return null;
     }
 
    // if (gameRecord.length < game){
-
+    
 
     
         //while the rounds are less than five keep playing until there are 5 rounds
@@ -92,36 +110,38 @@ function playRound(player){
         
         //logic for the game
         if (playerWin === computerWin){
-            console.log('It is a tie!')
+            outcome.innerHTML = ('It is a tie!');
         } else if( playerWin === 'rock' && computerWin === 'scissor'){
-            console.log('Player defeats computer with rock vs scissor');
+            outcome.innerHTML ='Player defeats computer with rock vs scissor';
             gameRecord.push('Player Wins!');
             playerScore++;
         } else if( playerWin === 'rock' && computerWin === 'paper'){
-            console.log('Computer defeats player with paper vs rock');
+            outcome.innerHTML ='Computer defeats player with paper vs rock';
             gameRecord.push('Player Loses!');
             computerScore++;
         } else if( playerWin === 'scissor' && computerWin === 'paper'){
-            console.log('Player defeats computer with scissor vs paper');
+            outcome.innerHTML ='Player defeats computer with scissor vs paper';
             gameRecord.push('Player Wins!');
             playerScore++;
         } else if( playerWin === 'scissor' && computerWin === 'rock'){
-            console.log('Computer defeats player with rock vs scissor');
+            outcome.innerHTML ='Computer defeats player with rock vs scissor';
             gameRecord.push('Player Loses!');
             computerScore++;
         }  else if( playerWin === 'paper' && computerWin === 'rock'){
-            console.log('Player defeats computer with paper vs rock');
+            outcome.innerHTML ='Player defeats computer with paper vs rock';
             gameRecord.push('Player Wins!');
             playerScore++;
         } else if( playerWin === 'paper' && computerWin === 'scissor'){
-            console.log('Computer defeats player with scissor vs paper');
+            outcome.innerHTML ='Computer defeats player with scissor vs paper';
             gameRecord.push('Player Loses!');
             computerScore++;
         }
 
-        console.log('Round '+ (gameRecord.length) + ' Player: ' + playerScore + ' : Computer: ' + computerScore +'.');
-        document.getElementById('userStats').innerHTML = 'Player Score : ' + playerScore + '!';
-        document.getElementById('computerStats').innerHTML = 'Computer Score : ' + computerScore + '!';
+       
+        userStat.innerHTML= 'Player Score: ' + playerScore;
+        computerStat.innerHTML= 'Computer Score: ' + computerScore;
+        roundStat.innerHTML = 'Round: '+ gameRecord.length + '&nbsp' + ' Player: ' + playerScore + ' : Computer: ' + computerScore +'.';
+
 
    // }
 
@@ -130,21 +150,27 @@ function playRound(player){
 
     
  //   console.log(gameRecord.length)
-    console.log( playerScore > computerScore ? 'Player has won the game!': 'Computer has won the game!');
+   //  playerScore > computerScore ? outcome.innerHTML = 'Player has won the game!': outcome.innerHTML = 'Computer has won the game!';
   //need to build logic for the live feed
-    if(playerScore > 3){
-        console.log('Best 3 out of 5. Game is done!')
-        document.getElementById('liveFeed').innerHTML = 'The winner is the Player : ' + playerScore + ' !';
+    if(playerScore >= game && (playerScore > computerScore)){
+        outcome.innerHTML ='Best ' + game + ' out of ' + (game + 2) +' Game is done!';
+        liveFeeder.innerHTML = 'The winner is the Player : ' + playerScore + ' !';
       
         // else if(computerScore > playerScore){
         //     document.getElementsByTagName('h3').textContent = 'The winner is the Computer: ' + computerScore + ' !';
         // }
         
     }
+    if(computerScore >= game && (playerScore < computerScore)){
+        outcome.innerHTML ='Best '+ game +' out of ' + (game + 2) + ' Game is done!';
+        liveFeeder.innerHTML = 'The winner is the Computer : ' + computerScore + ' !';
+
+        
+    }
 
 }
 
 
-console.log(playerSelection(bsign))
+//console.log(playerSelection(bsign))
 //logic for rock paper scissors
 
